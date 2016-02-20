a0a2b750014f2cd1f4b34fa0cf32ab76e1a48201
Sage <sage@Sages-MacBook-Air.local>
2016-02-20 11:38:15 -0600

    Working datatable

--- server.R
+++ server.R
@@ -2,11 +2,15 @@
 library(shinydashboard)
 library(shiny)
 library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(data.table)
+library(DT)
 
 server <- function(input, output) {
   
   order_data <- readRDS("./data/order_data.rds")
-  
+
   output$order_status <- renderText({
     order_status_data <- order_data[order_data$client == input$client_select,]
     paste("Completed: ", as.character(nrow(order_status_data[order_status_data$order_status == "Completed",])),
@@ -19,9 +23,25 @@ server <- function(input, output) {
     selectInput('client_select', label = 'Select Client', choices = client_list)
   })
   
-  output$plot1 <- renderPlot({
+  output$orders_placed_date <- renderPlot({
     plot_data <- order_data[order_data$client == input$client_select,]
-    ggplot(plot_data, aes(x = order_date, y = order_price)) +
-      geom_bar(stat = 'identity')
+    ggplot(plot_data, aes(x = order_date)) +
+      geom_bar() + xlab("Date Order Placed") + ylab("# of Orders Placed")
+  })
+  
+  output$orders_due_date <- renderPlot({
+    plot_data <- order_data[order_data$client == input$client_select,]
+    ggplot(plot_data, aes(x = due_date)) +
+      geom_bar() + xlab("Date Order Due") + ylab("# of Orders Due")
+  })
+  
+  output$next_orders <- DT::renderDataTable({
+    top5 <- order_data[order_data$client == input$client_select & order_data$order_status == "In Progress",]
+    top5 <- top5[order(top5$due_dat, decreasing = TRUE),][1:5,]
+    DT::datatable(top5, options = list(paging = FALSE, searching = FALSE))
+  })
+  
+  output$all_orders <- renderDataTable({
+    order_data[order(order_data$due_date, decreasing = TRUE), ]
   })
 }
--- ui.R
+++ ui.R
@@ -4,7 +4,8 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem('Dashboard', tabName = 'dashboard', icon = icon('dashboard')),
-      menuItem('Widgets', tabName = 'widgets', icon = icon('th')),
+      menuItem('Order List', tabName = 'order_list', icon = icon('th-list')),
+      menuItem('Client Map', tabName = 'client_map', icon = icon('map')),
       uiOutput('select_clients')
     )
   ),
@@ -12,18 +13,29 @@ ui <- dashboardPage(
     tabItems(
       tabItem(tabName = 'dashboard',
               fluidRow(
-                box(title = 'Order Status', status = 'danger', solidHeader = TRUE,
+                box(title = 'Order Status', status = 'danger', solidHeader = TRUE, width = 12, align = 'center',
                     h1(textOutput('order_status')))
               ),
               fluidRow(
-                box(title = 'Orders by Date', status = 'primary', solidHeader = TRUE,
-                    plotOutput('plot1', height = 250)),
-                textOutput('client_list')
+                box(title = 'Orders Placed', status = 'primary', solidHeader = TRUE,
+                    plotOutput('orders_placed_date', height = 250)),
+                
+                box(title = 'Orders Due', status = 'primary', solidHeader = TRUE,
+                    plotOutput('orders_due_date', height = 250))
+              ),
+              fluidRow(
+                box(title = 'Next 5 Orders Due', status = 'danger', solidHeader = TRUE, width = 12,
+                    dataTableOutput('next_orders'))
               )
       ),
-      tabItem(tabName = 'widgets',
-              h2('Widgets tab content')
-      )
+      tabItem(tabName = "order_list",
+              fluidRow(
+                box(title = 'All Orders', status = 'danger', solidHeader = TRUE, width = 12,
+                    dataTableOutput('all_orders'))
+              ))
+      # tabItem(tabName = 'widgets',
+      #         h2('Widgets tab content')
+      # )
     )
   )
 )
